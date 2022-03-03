988b985885a8f0a047e07d73482686f006363537
Patrick <patrickcannady@gmail.com>
2022-03-03 15:32:03 -0500

    commit tree 7998ca651404586389b0813eed461cab028e91fc

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if(someNumber <= '400'){
+    return 'This one is on me!';
+  } else if(someNumber > '2000' && someNumber <= '2500'){
+      return "I will gladly take your thirty bucks."
+  } else {
+     (someNumber >= '2501');
+    return "No can do.";
+  }}
+  
+
+function ternaryCheckCity(destination){
+  return destination === 'NYC'?'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(amount){
+  switch(amount){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  }
 }
